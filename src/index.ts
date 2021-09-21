fadc805f687ceb8b96f236ed546f6f1adc9a6c13
hansschenker <gian.schenker@gmail.com>
2021-09-21 13:38:34 +0200

    feat: add start stop with timer

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,24 @@
-import { Observable, of, Subject, BehaviorSubject, combineLatest, fromEvent, interval, merge} from "rxjs"
+import { Observable, of, Subject, BehaviorSubject, combineLatest, fromEvent, interval, merge, NEVER} from "rxjs"
 import { filter, map, mapTo, switchMap, mergeMap, scan, tap, withLatestFrom} from "rxjs/operators"
 
- 
-const runState = new BehaviorSubject<boolean>(false);  
-const stopState = new BehaviorSubject<boolean>(false);
-// // button refs: stop
-// const btnStop = document.getElementById("stop") as HTMLButtonElement;
-// const btnStopChanges = fromEvent(btnStop, "click").pipe(
+
+
+// // button refs: resume
+// const btnResume = document.getElementById("resume") as HTMLButtonElement;
+// const btnResumeChanges = fromEvent(btnResume, "click").pipe(
+//     map( (e:MouseEvent) => e.target['id'] )
+// )
+// // // button refs: reset
+// const btnReset = document.getElementById("reset") as HTMLButtonElement;
+// const btnResetChanges = fromEvent(btnReset, "click").pipe(
 //     map( (e:MouseEvent) => e.target['id'] )
 // )
 
-// // button refs: resume
-const btnResume = document.getElementById("resume") as HTMLButtonElement;
-const btnResumeChanges = fromEvent(btnResume, "click").pipe(
-    map( (e:MouseEvent) => e.target['id'] )
-)
-// // button refs: reset
-const btnReset = document.getElementById("reset") as HTMLButtonElement;
-const btnResetChanges = fromEvent(btnReset, "click").pipe(
-    map( (e:MouseEvent) => e.target['id'] )
-)
+// type PomodoroAction = "idle" | "run" | "stop" | "resume" | "reset"
 
-type PomodoroAction = "idle" | "run" | "stop" | "resume" | "reset"
+// type RunState = "run" | "stop";
 
-type RunState = "run" | "stop";
-
-const PomodoroVmFn = (vm: PomodoroVm) => vm;
+// const PomodoroVmFn = (vm: PomodoroVm) => vm;
 
 interface PomodoroVm  {
    running: boolean,
@@ -33,7 +26,6 @@ interface PomodoroVm  {
    paused: boolean,
    countdown:  number,
    current: number,
-   // button ref: run
 }
 const initialVm : PomodoroVm = {
     running: false,
@@ -43,58 +35,47 @@ const initialVm : PomodoroVm = {
     current: 2500,
 }
 
+// button ref: run
+const runState = new BehaviorSubject<number>(0);
 const btnRun = document.getElementById("run") as HTMLButtonElement;
 const runClicks = fromEvent(btnRun, "click").pipe(
-    // tap(v => console.log("runClick:", v)),
-    mapTo(true),
-    ).pipe(
-        tap( v => runState.next(v))
-    )
-    //.subscribe();
-
-
-// btnRun.addEventListener("click", e => {
-//     runState.next( true);
-//     btnRun.innerText = "Stop";
-// })
+    map(v => interval(1000))
+)
 
 
 // button ref: stop
-
+const stopState = new BehaviorSubject<number>(0);
 const btnStop = document.getElementById("stop") as HTMLButtonElement;
 const stopClicks = fromEvent(btnStop, "click")
 .pipe(
-    // tap(v => console.log("runClick:", v)),
-    mapTo(false),
-    ).pipe(
-        tap( v => runState.next(v))
-    )
-    //.subscribe();
+    map( v => of(0))
+)
 
-const btnClicks = merge(runClicks , stopClicks);
+const btnClicks = merge(runClicks , stopClicks).pipe(
+    mergeMap( v => v)
+)
 
-
-
-
-///////////////////////7
 
 
 const runChanges = runState.pipe(
-    tap( v => console.log("runChange:",v)),
-    map( (delta: boolean) => (vm:PomodoroVm) => ({...vm, current:vm.current-1, runtext:"Pause"}) ),
+    // tap( v => console.log("runChange:",v)),
+    map( (delta: number) => (vm:PomodoroVm) => ({...vm, current:vm.current - 1, runtext:"Pause"}) ),
   );
 
   const stopChanges = stopState.pipe(
-    tap( v => console.log("stopChange:",v)),
-    map( (delta: boolean) => (vm:PomodoroVm) => ({...vm,runtext: "Start" }) )
+    // tap( v => console.log("stopChange:",v)),
+    map( (delta: number) => (vm:PomodoroVm) => ({...vm,runtext: "Start" }) )
   );
 
-  const changes = btnClicks.pipe(
-      switchMap( (v:boolean) => v? runChanges : stopChanges )
+  const runOrstopChanges = btnClicks.pipe(
+      switchMap( (v) => v===1? runChanges : stopChanges ),
+      tap( (v) => console.log("run or stop:", v))
   )
   
-  const vmChanges = changes.pipe(
+  const vmChanges = runOrstopChanges.pipe(
+    tap( v => console.log(v)),
     //   tap( (run, stop) => console.log("run, stop:", run, stop)),
     scan((oldVm: PomodoroVm, mutateFn:(vm: PomodoroVm) => PomodoroVm) => mutateFn(oldVm), initialVm as PomodoroVm)
     // scan( (prevVm:PomodoroVm, mutationFn:(vm:PomodoroVm)=>PomodoroVm) => mutationFn(prevVm), {} as PomodoroVm)
-  ).subscribe( v => console.log(v))
+  )
+  //.subscribe( v => console.log(v))
